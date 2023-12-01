252df56b3e8abf3dbff7ce503e25cf5fd1c12af9
Stephen Bolton <stephen@bitsonthemind.com>
2023-12-01 03:06:47 -0500

    Day1 - Clean up
    Refactored code and got both versions running sharing code

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,4 +1,6 @@
 defmodule AdventOfCode.Day01 do
+  defguard is_digit(number) when number in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
   def part1(input) do
     input
     |> String.trim()
@@ -6,14 +8,15 @@ defmodule AdventOfCode.Day01 do
     |> Enum.reduce(0, fn row, count ->
       row
       |> String.replace(~r|\D|, "", global: true)
+      |> String.codepoints()
+      |> scan_string({_first = nil, _last = nil})
       |> case do
-        number when length(number) == 1 ->
-          number
+        {first, nil} ->
+          String.to_integer("#{first}#{first}")
 
-        number ->
-          String.at(number, 0) <> String.last(number)
+        {first, last} ->
+          String.to_integer("#{first}#{last}")
       end
-      |> String.to_integer()
       |> Kernel.+(count)
     end)
   end
@@ -37,8 +40,11 @@ defmodule AdventOfCode.Day01 do
     end)
   end
 
-  def scan_string(["1" | rest], {nil, nil}), do: scan_string(rest, {1, nil})
-  def scan_string(["1" | rest], {first, _}), do: scan_string(rest, {first, 1})
+  def scan_string([num | rest], {nil, nil}) when is_digit(num),
+    do: scan_string(rest, {num, nil})
+
+  def scan_string([num | rest], {first, _}) when is_digit(num),
+    do: scan_string(rest, {first, num})
 
   def scan_string(["o", "n", "e" | rest], {nil, nil}),
     do: scan_string(["n", "e" | rest], {1, nil})
@@ -46,17 +52,11 @@ defmodule AdventOfCode.Day01 do
   def scan_string(["o", "n", "e" | rest], {first, _}),
     do: scan_string(["n", "e" | rest], {first, 1})
 
-  def scan_string(["2" | rest], {nil, nil}), do: scan_string(rest, {2, nil})
-  def scan_string(["2" | rest], {first, _}), do: scan_string(rest, {first, 2})
-
   def scan_string(["t", "w", "o" | rest], {nil, nil}),
     do: scan_string(["w", "o" | rest], {2, nil})
 
   def scan_string(["t", "w", "o" | rest], {first, _}),
     do: scan_string(["w", "o" | rest], {first, 2})
-
-  def scan_string(["3" | rest], {nil, nil}), do: scan_string(rest, {3, nil})
-  def scan_string(["3" | rest], {first, _}), do: scan_string(rest, {first, 3})
 
   def scan_string(["t", "h", "r", "e", "e" | rest], {nil, nil}),
     do: scan_string(["h", "r", "e", "e" | rest], {3, nil})
@@ -64,17 +64,11 @@ defmodule AdventOfCode.Day01 do
   def scan_string(["t", "h", "r", "e", "e" | rest], {first, _}),
     do: scan_string(["h", "r", "e", "e" | rest], {first, 3})
 
-  def scan_string(["4" | rest], {nil, nil}), do: scan_string(rest, {4, nil})
-  def scan_string(["4" | rest], {first, _}), do: scan_string(rest, {first, 4})
-
   def scan_string(["f", "o", "u", "r" | rest], {nil, nil}),
     do: scan_string(["o", "u", "r" | rest], {4, nil})
 
   def scan_string(["f", "o", "u", "r" | rest], {first, _}),
     do: scan_string(["o", "u", "r" | rest], {first, 4})
-
-  def scan_string(["5" | rest], {nil, nil}), do: scan_string(rest, {5, nil})
-  def scan_string(["5" | rest], {first, _}), do: scan_string(rest, {first, 5})
 
   def scan_string(["f", "i", "v", "e" | rest], {nil, nil}),
     do: scan_string(["i", "v", "e" | rest], {5, nil})
@@ -82,17 +76,11 @@ defmodule AdventOfCode.Day01 do
   def scan_string(["f", "i", "v", "e" | rest], {first, _}),
     do: scan_string(["i", "v", "e" | rest], {first, 5})
 
-  def scan_string(["6" | rest], {nil, nil}), do: scan_string(rest, {6, nil})
-  def scan_string(["6" | rest], {first, _}), do: scan_string(rest, {first, 6})
-
   def scan_string(["s", "i", "x" | rest], {nil, nil}),
     do: scan_string(["i", "x" | rest], {6, nil})
 
   def scan_string(["s", "i", "x" | rest], {first, _}),
     do: scan_string(["i", "x" | rest], {first, 6})
-
-  def scan_string(["7" | rest], {nil, nil}), do: scan_string(rest, {7, nil})
-  def scan_string(["7" | rest], {first, _}), do: scan_string(rest, {first, 7})
 
   def scan_string(["s", "e", "v", "e", "n" | rest], {nil, nil}),
     do: scan_string(["e", "v", "e", "n" | rest], {7, nil})
@@ -100,17 +88,11 @@ defmodule AdventOfCode.Day01 do
   def scan_string(["s", "e", "v", "e", "n" | rest], {first, _}),
     do: scan_string(["e", "v", "e", "n" | rest], {first, 7})
 
-  def scan_string(["8" | rest], {nil, nil}), do: scan_string(rest, {8, nil})
-  def scan_string(["8" | rest], {first, _}), do: scan_string(rest, {first, 8})
-
   def scan_string(["e", "i", "g", "h", "t" | rest], {nil, nil}),
     do: scan_string(["i", "g", "h", "t" | rest], {8, nil})
 
   def scan_string(["e", "i", "g", "h", "t" | rest], {first, _}),
     do: scan_string(["i", "g", "h", "t" | rest], {first, 8})
-
-  def scan_string(["9" | rest], {nil, nil}), do: scan_string(rest, {9, nil})
-  def scan_string(["9" | rest], {first, _}), do: scan_string(rest, {first, 9})
 
   def scan_string(["n", "i", "n", "e" | rest], {nil, nil}),
     do: scan_string(["i", "n", "e" | rest], {9, nil})
