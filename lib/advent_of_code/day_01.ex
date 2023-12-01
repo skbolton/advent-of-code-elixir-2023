bf0d1bcdd0cd05c16a3a027a3d59369ca1e955ec
Stephen Bolton <stephen@bitsonthemind.com>
2023-12-01 13:25:12 -0500

    Day 1 - More cleanup

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -46,59 +46,59 @@ defmodule AdventOfCode.Day01 do
   def scan_string([num | rest], {first, _}) when is_digit(num),
     do: scan_string(rest, {first, num})
 
-  def scan_string(["o", "n", "e" | rest], {nil, nil}),
-    do: scan_string(["n", "e" | rest], {1, nil})
+  def scan_string(["o", "n", "e" | _] = list, {nil, nil}),
+    do: scan_string(tl(list), {1, nil})
 
-  def scan_string(["o", "n", "e" | rest], {first, _}),
-    do: scan_string(["n", "e" | rest], {first, 1})
+  def scan_string(["o", "n", "e" | _] = list, {first, _}),
+    do: scan_string(tl(list), {first, 1})
 
-  def scan_string(["t", "w", "o" | rest], {nil, nil}),
-    do: scan_string(["w", "o" | rest], {2, nil})
+  def scan_string(["t", "w", "o" | _] = list, {nil, nil}),
+    do: scan_string(tl(list), {2, nil})
 
-  def scan_string(["t", "w", "o" | rest], {first, _}),
-    do: scan_string(["w", "o" | rest], {first, 2})
+  def scan_string(["t", "w", "o" | _] = list, {first, _}),
+    do: scan_string(tl(list), {first, 2})
 
-  def scan_string(["t", "h", "r", "e", "e" | rest], {nil, nil}),
-    do: scan_string(["h", "r", "e", "e" | rest], {3, nil})
+  def scan_string(["t", "h", "r", "e", "e" | _] = list, {nil, nil}),
+    do: scan_string(tl(list), {3, nil})
 
-  def scan_string(["t", "h", "r", "e", "e" | rest], {first, _}),
-    do: scan_string(["h", "r", "e", "e" | rest], {first, 3})
+  def scan_string(["t", "h", "r", "e", "e" | _] = list, {first, _}),
+    do: scan_string(tl(list), {first, 3})
 
-  def scan_string(["f", "o", "u", "r" | rest], {nil, nil}),
-    do: scan_string(["o", "u", "r" | rest], {4, nil})
+  def scan_string(["f", "o", "u", "r" | _] = list, {nil, nil}),
+    do: scan_string(tl(list), {4, nil})
 
-  def scan_string(["f", "o", "u", "r" | rest], {first, _}),
-    do: scan_string(["o", "u", "r" | rest], {first, 4})
+  def scan_string(["f", "o", "u", "r" | _] = list, {first, _}),
+    do: scan_string(tl(list), {first, 4})
 
-  def scan_string(["f", "i", "v", "e" | rest], {nil, nil}),
-    do: scan_string(["i", "v", "e" | rest], {5, nil})
+  def scan_string(["f", "i", "v", "e" | _] = list, {nil, nil}),
+    do: scan_string(tl(list), {5, nil})
 
-  def scan_string(["f", "i", "v", "e" | rest], {first, _}),
-    do: scan_string(["i", "v", "e" | rest], {first, 5})
+  def scan_string(["f", "i", "v", "e" | _] = list, {first, _}),
+    do: scan_string(tl(list), {first, 5})
 
-  def scan_string(["s", "i", "x" | rest], {nil, nil}),
-    do: scan_string(["i", "x" | rest], {6, nil})
+  def scan_string(["s", "i", "x" | _] = list, {nil, nil}),
+    do: scan_string(tl(list), {6, nil})
 
-  def scan_string(["s", "i", "x" | rest], {first, _}),
-    do: scan_string(["i", "x" | rest], {first, 6})
+  def scan_string(["s", "i", "x" | _] = list, {first, _}),
+    do: scan_string(tl(list), {first, 6})
 
-  def scan_string(["s", "e", "v", "e", "n" | rest], {nil, nil}),
-    do: scan_string(["e", "v", "e", "n" | rest], {7, nil})
+  def scan_string(["s", "e", "v", "e", "n" | _] = list, {nil, nil}),
+    do: scan_string(tl(list), {7, nil})
 
-  def scan_string(["s", "e", "v", "e", "n" | rest], {first, _}),
-    do: scan_string(["e", "v", "e", "n" | rest], {first, 7})
+  def scan_string(["s", "e", "v", "e", "n" | _] = list, {first, _}),
+    do: scan_string(tl(list), {first, 7})
 
-  def scan_string(["e", "i", "g", "h", "t" | rest], {nil, nil}),
-    do: scan_string(["i", "g", "h", "t" | rest], {8, nil})
+  def scan_string(["e", "i", "g", "h", "t" | _] = list, {nil, nil}),
+    do: scan_string(tl(list), {8, nil})
 
-  def scan_string(["e", "i", "g", "h", "t" | rest], {first, _}),
-    do: scan_string(["i", "g", "h", "t" | rest], {first, 8})
+  def scan_string(["e", "i", "g", "h", "t" | _] = list, {first, _}),
+    do: scan_string(tl(list), {first, 8})
 
-  def scan_string(["n", "i", "n", "e" | rest], {nil, nil}),
-    do: scan_string(["i", "n", "e" | rest], {9, nil})
+  def scan_string(["n", "i", "n", "e" | _] = list, {nil, nil}),
+    do: scan_string(tl(list), {9, nil})
 
-  def scan_string(["n", "i", "n", "e" | rest], {first, _}),
-    do: scan_string(["i", "n", "e" | rest], {first, 9})
+  def scan_string(["n", "i", "n", "e" | _] = list, {first, _}),
+    do: scan_string(tl(list), {first, 9})
 
   def scan_string([_ | rest], numbers), do: scan_string(rest, numbers)
   def scan_string([], numbers), do: numbers
