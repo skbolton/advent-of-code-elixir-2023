8d54c685fb4ddefc7e3111c6507ce7753bf99508
Stephen Bolton <stephen@bitsonthemind.com>
2023-12-09 20:45:47 -0500

    Day 6 - P1

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,40 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  import AdventOfCode.Helpers
+
+  def part1(input) do
+    input
+    |> parse()
+    |> Enum.map(&find_winning_runs/1)
+    |> Enum.product()
   end
 
   def part2(_args) do
+  end
+
+  def parse(input) do
+    input
+    |> split_lines()
+    |> Enum.map(fn line ->
+      line
+      |> String.split(" ", trim: true)
+      |> tl()
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.zip()
+  end
+
+  def find_winning_runs({time, score_to_beat}) do
+    1..time
+    |> Enum.to_list()
+    |> Enum.reduce({_win_count = 0, _speed = 0}, fn tick, {win_count, speed} ->
+      rounds_left = time - tick + 1
+
+      if speed * rounds_left > score_to_beat do
+        {win_count + 1, speed + 1}
+      else
+        {win_count, speed + 1}
+      end
+    end)
+    |> elem(0)
   end
 end
--- test/advent_of_code/day_06_test.exs
+++ test/advent_of_code/day_06_test.exs
@@ -3,12 +3,13 @@ defmodule AdventOfCode.Day06Test do
 
   import AdventOfCode.Day06
 
-  @tag :skip
   test "part1" do
-    input = nil
-    result = part1(input)
+    input = """
+    Time:      7  15   30
+    Distance:  9  40  200
+    """
 
-    assert result
+    assert 288 = part1(input)
   end
 
   @tag :skip
