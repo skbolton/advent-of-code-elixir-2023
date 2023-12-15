16a210d79ccc82fdb427665bf25848b82431fa4c
Stephen Bolton <stephen@bitsonthemind.com>
2023-12-15 01:16:59 -0500

    Day 8 - Part 1

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,5 +1,40 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  import AdventOfCode.Helpers
+
+  def part1(input) do
+    [steps, nodes] = String.split(input, "\n\n", trim: true)
+
+    graph =
+      nodes
+      |> split_lines()
+      |> Enum.map(&String.split(&1, " = "))
+      |> Enum.map(fn [location, connections] ->
+        [
+          location,
+          connections
+          |> String.replace(~r/\(|\)/, "")
+          |> String.split(", ")
+        ]
+      end)
+      |> Enum.map(fn [node, [left, right]] -> {node, {left, right}} end)
+      |> Enum.into(Map.new())
+
+    steps
+    |> String.split("", trim: true)
+    |> Stream.cycle()
+    |> Stream.transform({graph, "AAA"}, fn
+      _step, {_graph, "ZZZ"} = acc ->
+        {:halt, acc}
+
+      "L", {graph, location} ->
+        {new_location, _right} = Map.get(graph, location)
+        {[1], {graph, new_location}}
+
+      "R", {graph, location} ->
+        {_left, new_location} = Map.get(graph, location)
+        {[1], {graph, new_location}}
+    end)
+    |> Enum.sum()
   end
 
   def part2(_args) do
