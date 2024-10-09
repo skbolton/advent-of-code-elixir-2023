992d90aad99e588b7114e60c6a2ad0c2c9cbf0c4
Stephen Bolton <stephen@bitsonthemind.com>
2024-10-09 15:56:24 -0400

    Day 9 - Move to stream
    Instead of having to continually build up lists in memory we can stream
    item by item and process things in a more memory efficient way.

--- lib/advent_of_code/day_09.ex
+++ lib/advent_of_code/day_09.ex
@@ -4,24 +4,24 @@ defmodule AdventOfCode.Day09 do
   def part1(input) do
     input
     |> split_lines()
-    |> Enum.map(fn line ->
+    |> Stream.map(fn line ->
       line
       |> String.split(" ")
       |> Enum.map(&String.to_integer/1)
     end)
-    |> Enum.map(&build_sequence_end/1)
+    |> Stream.map(&build_sequence_end/1)
     |> Enum.sum()
   end
 
   def part2(input) do
     input
     |> split_lines()
-    |> Enum.map(fn line ->
+    |> Stream.map(fn line ->
       line
       |> String.split(" ")
       |> Enum.map(&String.to_integer/1)
     end)
-    |> Enum.map(&build_sequence_begin/1)
+    |> Stream.map(&build_sequence_begin/1)
     |> Enum.sum()
   end
 
